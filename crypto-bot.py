7eb95bb929228bb259db21a6c9d6825d9b529d64
Federico Francescut <federico.francescut@gmail.com>
2021-10-28 18:20:32 +0200

    Add ta lib and config.py
    removed all test

--- crypto-bot.py
+++ crypto-bot.py
@@ -4,8 +4,25 @@ import time
 from datetime import datetime
 from pprint import pprint
 
+import config
 import ccxt
+import pandas as pd
+from ta import add_all_ta_features
+from ta.utils import dropna
+from ta.volatility import BollingerBands, AverageTrueRange
 
-exchange = ccxt.binance({})
+exchange = ccxt.binance({
+    'apiKey': config.BINANCE_API_KEY,
+    'secret': config.BINANCE_SECRET_KEY
+})
 
-print(exchange.fetch_ticker("BTC/EUR"))
+print(exchange.fetch_ticker("BTC/EUR")['last'])
+
+bars = exchange.fetch_ohlcv('ETH/EUR', limit=20)
+
+for bar in bars:
+    print(bar)
+
+df = pd.DataFrame(bars, columns=['timestamp','high','low','close','volume'])
+print(df)
+#bb_indicator = BollingerBands()
